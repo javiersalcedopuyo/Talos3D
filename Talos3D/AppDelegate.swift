39cce6e1f9be1389a91465c7e08e5b10be29ffe5
Javier Salcedo <javiersalcedo.p@gmail.com>
2021-12-30 13:53:56 +0100

    Initial Commit

---
+++ Talos3D/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Talos3D
+//
+//  Created by Javier Salcedo on 30/12/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
