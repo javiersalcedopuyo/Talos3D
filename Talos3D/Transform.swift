fbbce13b1b442890b8709cdc7431b3d17478dd81
Javier Salcedo <javiersalcedo.p@gmail.com>
2022-01-04 17:57:49 +0100

    Add Transform.getLocalToWorldMatrix()
    Also correct handedness issue in rotate()

--- Talos3D/Transform.swift
+++ Talos3D/Transform.swift
@@ -21,14 +21,26 @@ class Transform
         position = Vector3.zero()
         scale    = Vector3.identity()
 
-        forward  = Vector3(x:0, y:0, z:1)
-        up       = Vector3(x:0, y:1, z:0)
         right    = Vector3(x:1, y:0, z:0)
+        up       = Vector3(x:0, y:1, z:0)
+        forward  = Vector3(x:0, y:0, z:1)
     }
 
     public func getForward() -> Vector3 { return self.forward }
     public func getUp()      -> Vector3 { return self.up }
     public func getRight()   -> Vector3 { return self.right }
+
+    public func getLocalToWorldMatrix() -> Matrix4x4
+    {
+        let X = Vector4(self.right,    0)
+        let Y = Vector4(self.up,       0)
+        let Z = Vector4(self.forward,  0)
+        let T = Vector4(self.position, 1)
+
+        return Matrix4x4(a: X, b: Y, c: Z, d: T)
+    }
+
+    // TODO: public func getWorldToLocaldMatrix() -> Matrix4x4
 
     // TODO: public func getEulerAngles() -> Vector3 { return self.eulerAngles }
     // TODO: public func getRotationQuaternion() -> Quaternion { return self.rotQuaternion }
@@ -51,8 +63,8 @@ class Transform
         let R = rotX * rotY * rotZ
 
         self.forward = (R * self.forward).normalized() // Is it really necessary to normalize?
-        self.right   = self.forward.cross( Vector3(x:0, y:1, z:0) )
-        self.up      = self.right.cross( self.forward )
+        self.right   = self.forward.cross( Vector3(x:0, y:-1, z:0) )
+        self.up      = self.forward.cross( self.right )
     }
     // TODO: public func rotate(q: Quaterion)
     // TODO: public func lookAt(target: Vector3)
@@ -64,6 +76,4 @@ class Transform
 
     // TODO: private var eulerAngles: Vector3
     // TODO: private var rotQuaternion: Quaternion
-    // TODO: private var localToWorldMatrix: Matrix4x4 // Maybe construct it on demand?
-    // TODO: private var worldToLocalMatrix: Matrix4x4
 }
